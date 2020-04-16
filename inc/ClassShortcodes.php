d735fe04d7fe8b3206586a68b67a16a7a4a08f6c
Sanjeev Kumar <upworksanjeev@gmail.com>
2020-04-16 14:16:50 +0530

    Update ClassShortcodes.php
    Change Constant array to method

--- inc/ClassShortcodes.php
+++ inc/ClassShortcodes.php
@@ -12,7 +12,8 @@ class UtmvGrabberProShortcodes {
          * Shortcode to display the list of utm variables in front end through shortcodes
          * [utm_varibles]
          */		
-		$utmFields = UTMV_GRABBER_UTM_FIELDS;
+		//$utmFields = UTMV_GRABBER_UTM_FIELDS;
+		$utmFields = UtmvGrabber()->engine->getShortcode(); 
 		$this->utmWpFormShortcodes($utmFields);		
     }
 
@@ -23,21 +24,23 @@ class UtmvGrabberProShortcodes {
      */
     private function utmWpFormShortcodes($utmFields) {
 		$cfield = '';
-        foreach ($utmFields as $id=>$utmfield){
-			if (isset($_GET[$utmfield]) && $_GET[$utmfield] != '')
-				$cfield = htmlspecialchars($_GET[$utmfield],ENT_QUOTES, 'UTF-8');
-			elseif(isset($_COOKIE[$utmfield]) && $_COOKIE[$utmfield] != ''){ 
-				$cfield = $_COOKIE[$utmfield];
-			}else{
-				$cfield = '';
-			}		
-			$_COOKIE[$utmfield] = $cfield;
-			
-			/* add shortcodes for ninja forms */
-			add_shortcode('nj_'.$utmfield, function() use ($utmfield) {return urldecode($_COOKIE[$utmfield]);});	
-			
-			/* display utm filed value in gravity forms mail */
-			add_filter( 'gform_field_value_'.$utmfield, function() use ($utmfield) {return urldecode($_COOKIE[$utmfield]); });			
+		if(!empty($utmFields)){ 
+			foreach ($utmFields as $id=>$utmfield){
+				if (isset($_GET[$utmfield]) && $_GET[$utmfield] != '')
+					$cfield = htmlspecialchars($_GET[$utmfield],ENT_QUOTES, 'UTF-8');
+				elseif(isset($_COOKIE[$utmfield]) && $_COOKIE[$utmfield] != ''){ 
+					$cfield = $_COOKIE[$utmfield];
+				}else{
+					$cfield = '';
+				}		
+				$_COOKIE[$utmfield] = $cfield;
+				
+				/* add shortcodes for ninja forms */
+				add_shortcode('nj_'.$utmfield, function() use ($utmfield) {return urldecode($_COOKIE[$utmfield]);});	
+				
+				/* display utm filed value in gravity forms mail */
+				add_filter( 'gform_field_value_'.$utmfield, function() use ($utmfield) {return urldecode($_COOKIE[$utmfield]); });			
+			}
 		}
     }
 }
